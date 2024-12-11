4fa58b934b71662cde9e7bac9e66ae32347b93e5
luchenspace <behancechen@gmail.com>
2024-12-12 00:14:08 +1300

    Add new route and lighting settings

--- src/components/route.tsx
+++ src/components/route.tsx
@@ -1,7 +1,61 @@
-import { CatmullRomCurve3 } from 'three';
-
 import { Line } from '@react-three/drei';
+import * as THREE from 'three';
+import { stations } from './stations';
 
-export default function RouteLine({ curve }: { curve: CatmullRomCurve3 }) {
-  return <Line points={curve.getPoints(100)} color='red' lineWidth={2} />;
+const SCALE_FACTOR = 10;
+
+function DebugPoints({ points, color, visible }: { points: THREE.Vector3[]; color: string; visible?: boolean }) {
+  return (
+    <>
+      {visible &&
+        points.map((point, index) => (
+          <mesh key={index} position={point}>
+            <sphereGeometry args={[1, 16, 16]} />
+            <meshBasicMaterial color={color} />
+          </mesh>
+        ))}
+    </>
+  );
 }
+
+export default function RouteLine({ visible = true }: { visible?: boolean }) {
+  const routeCurve = new THREE.CatmullRomCurve3(
+    stations.map(
+      (station) =>
+        new THREE.Vector3(
+          station.x * SCALE_FACTOR,
+          station.y + 33,
+          station.z * SCALE_FACTOR
+        )
+    ),
+    true
+  );
+  routeCurve.tension = 0.4;
+
+  const points = routeCurve.getPoints(1000);
+
+  return (
+    <>
+      {visible && (
+        <>
+          <Line points={points} color="blue" lineWidth={5} dashed={false} />
+          <DebugPoints
+            points={stations.map(
+              (station) =>
+                new THREE.Vector3(
+                  station.x * SCALE_FACTOR,
+                  station.y + 33,
+                  station.z * SCALE_FACTOR
+                )
+            )}
+            color="red"
+            visible={visible}
+          />
+          <DebugPoints points={points} color="black" visible={visible} />
+        </>
+      )}
+    </>
+  );
+}
+
+export { SCALE_FACTOR };
--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,22 +1,14 @@
 'use client';
-import { OrbitControls } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
-import { useEffect } from 'react';
 
 export default function Scene() {
-  const { camera } = useThree();
-
-  useEffect(() => {
-    camera.position.set(100, 90, 100);
-  }, [camera]);
-
   return (
     <>
-      <gridHelper args={[1000, 1000]} />
-      <OrbitControls />
-      <axesHelper args={[100]} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 10]} intensity={3} />
+      {/* Lighting and helper tools */}
+      <ambientLight intensity={1.6} />
+      <directionalLight position={[10, 20, 10]} intensity={2} castShadow />
+      <directionalLight position={[-10, 20, -10]} intensity={1.5} />
+      <directionalLight position={[-0, 20, 10]} intensity={1.7} />
+      <hemisphereLight color={'#ffffff'} groundColor={'#444444'} intensity={1.2} />
     </>
   );
 }
