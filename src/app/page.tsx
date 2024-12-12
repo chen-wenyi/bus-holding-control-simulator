b2b7cf45bf18642fc705122501a3505e553c0daf
wenyichen <chwwyi@163.com>
2024-12-12 16:57:19 +1300

    chore: merge changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Simulator from "@/components/simulator";
 
 export default function Home() {
   return (
@@ -5,7 +6,7 @@ export default function Home() {
       className='canvas-container'
       style={{ height: '100vh', width: '100vw' }}
     >
-      {/* <Simulator /> */}
+      <Simulator />
     </div>
   );
 }
